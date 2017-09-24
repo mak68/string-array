1eb330c1ec9165a1f343a9c37cbaa4b0ea8b92e3
Milena A Kosc <mak68@afsconnect1.njit.edu>
2017-09-24 14:38:00 -0400

    Added printString

--- index.php
+++ index.php
@@ -4,8 +4,10 @@ $text= "Pretty women wonder where my secret lies.</br>
 I'm not cute or built to suit a fashion model's size </br>
 But when I start to tell them,</br>
 They think I'm telling lies.</br>";
+$text2="I say it's in the reach of my arms";
 $obj->printthis($text);
-
+$obj->printString($text2);
+//$obj->stringLenght($text2)
 class main {
 
 	public function __construct ()  {
@@ -18,6 +20,12 @@ class main {
 	echo '<hr>';
 	}
 	
+	public function printString($text2) {
+	echo '<h3> Part 2 </h3>';
+	print ($text2);
+	echo '<hr>';
+	}
+
 	public function __deconstruct () {
 						}
 
